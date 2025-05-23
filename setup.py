9784f782d7dc0adecabde92cc9ec78bb9fe3bf17
Hiren Soni <65155846+hirensoni913@users.noreply.github.com>
2025-05-23 11:43:49 +0200

    Update setup.py
    Fixed scikit-learn and pandas version

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@ setup(
         'django-db-signals',
         'djangorestframework',
         'joblib',
-        'scikit-learn==0.23.2',
+        'scikit-learn==1.6.1',
         'xgboost==1.3.3',
         'category_encoders',
-        'pandas==1.1.4',
+        'pandas==2.2.3',
         'pandarallel==1.5.5',
         'gevent==21.12.0'
     ],
